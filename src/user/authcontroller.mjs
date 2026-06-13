35dbc009a4ccf034eb45cb0d4ed2684e1cdb8ead
Arunkumar1321 <akmirror619@gmail.com>
2026-06-13 21:48:27 +0530

    first commit

--- src/user/authcontroller.mjs
+++ src/user/authcontroller.mjs
@@ -25,24 +25,27 @@ login_post : async(req,res)=>{
 res.status(200).send({user:user.username,token})
 },
 
-signin_post : async(req,res)=>{
- const result = validationResult(req)
- if(!result.isEmpty()){
-   return res.status(400).send({errors:result.array()[0].msg})
- }
- const body = matchedData(req)
- body.password=await hashpassword(body.password)
- const euser= await User.findOne({username:body.username})
- if(euser){
-    return res.status(400).send({error:"User already exist,Go back to Login"})
- }
- const nuser=await User.create({
-    username:body.username,
-    password:body.password,
-    role: "user"
- })
- const token = generateToken(nuser._id)
- res.status(201).send({user:nuser.username,token})
-},
-}
-export default Authcontroller
+signin_post: async (req, res) => {
+    try {                                         
+        const result = validationResult(req)
+        if (!result.isEmpty()) {
+            return res.status(400).send({ errors: result.array()[0].msg })
+        }
+        const body = matchedData(req)
+        body.password = await hashpassword(body.password)
+        const euser = await User.findOne({ username: body.username })
+        if (euser) {
+            return res.status(400).send({ error: "User already exists, go back to Login" })
+        }
+        const nuser = await User.create({
+            username: body.username,
+            password: body.password,
+            role: "user"
+        })
+        const token = generateToken(nuser._id)
+        res.status(201).send({ user: nuser.username, token })
+    } catch (err) {                              
+        console.log("Signin error:", err.message)
+        res.status(500).send({ error: err.message })
+    }
+}}
